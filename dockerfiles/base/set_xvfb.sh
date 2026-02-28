355e146ba62d9579015b0fa8e62055b20bee7fbd
1Suro1 <timu.zaxarov@gmail.com>
2026-03-01 00:18:49 +0900

    add vscode

--- dockerfiles/base/set_xvfb.sh
+++ dockerfiles/base/set_xvfb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -x
-export DISPLAY=:99.0
+
+# Start Xvfb in background
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-#sleep 3
-set +x
+
+# Wait for Xvfb to start
+sleep 1
+
+# Execute the command passed to the container
 exec "$@"
